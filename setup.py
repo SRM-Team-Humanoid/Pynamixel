af54e38f5ad53880b7c7ec9daa1377f12c113365
arpit-kapoor <arpitkapur.dps@gmail.com>
2018-05-25 20:27:49 +0530

    minor fix

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='mydynamixel',
+    name='pynamixel',
     version='1.0.1',
-    packages=['mydynamixel'],
+    packages=['pynamixel'],
     url='',
     license='',
     author='arpitkapoor',
-    author_email='',
-    description=''
+    author_email='kapoor.arpit97@gmail.com',
+    description='A python module to Control Robotis Dynamixel Smart servos'
 )
