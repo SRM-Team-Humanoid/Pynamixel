b31110075075998345cb013ac4099a2a876ac61c
Arpit Kapoor <kapoor.arpit97@gmail.com>
2018-10-01 23:01:13 +0530

    updated for dynamixel sdk 3.6.0

--- pynamixel/io.py
+++ pynamixel/io.py
@@ -3,28 +3,30 @@
 
 import os, ctypes
 import protocol1_0, protocol2_0
-import dynamixel_functions as dxl
+from dynamixel_sdk import *
 
 class Dxl_IO(object):
-    def __init__(self, baudrate=1000000, port='/dev/ttyUSB0', protocol = 1):
+    # Initializer for Dynamixel I/O object
+    def __init__(self, baudrate=1000000, port='/dev/ttyUSB0', protocol = 1.0):
         self.baudrate = baudrate
-        self.port = dxl.portHandler(port.encode('utf-8'))
+        self.portHandler = PortHandler(port)
         self.protocol = protocol
-        if self.protocol == 1:
-            chain = protocol1_0.Chain(port = self.port)
-        elif self.protocol == 2:
-            chain = protocol2_0.Chain(port = self.port)
-        dxl.packetHandler()
+        self.packetHandler = PacketHandler(float(self.protocol))
+        if self.protocol == 1.0:
+            chain = protocol1_0.Chain(port=self.port, portHandler=self.portHandler, packetHandler=self.packetHandler)
+        elif self.protocol == 2.0:
+            chain = protocol2_0.Chain(port=self.port, portHandler=self.portHandler, packetHandler=self.packetHandler)
         self.connect()
 
+    # Open the Port and set the baudrate
     def connect(self):
-        if dxl.openPort(self.port):
-            dxl.setBaudRate(self.port, self.baudrate)
-        else:
+        if not self.portHandler.openPort():
             print("Failed to open the port!")
             quit()
+        if  not self.portHandler.setBaudRate(self.baudrate):
+            print("Failed to change the baudrate!")
+            quit()
 
+    # Close the Port
     def disconnect(self):
-        dxl.closePort(self.port)
-
-
+        portHandler.closePort(self.port)
--- pynamixel/protocol1_0.py
+++ pynamixel/protocol1_0.py
@@ -5,115 +5,109 @@ import os, ctypes
 from reg1_0 import *
 from ports import *
 from constants import *
-
-
-import dynamixel_functions as dxl
+from dynamixel_sdk import *
 
 
 #Daisy chain IO class for protocol 1
 class Chain(object):
-    def __init__(self, port):
+    def __init__(self, port, port_handler, packet_handler):
         self.port = port
         self.protocol = 1
         self.mx_res = MX_RESOLUTION # MX-28 Resolution
         self.fsr_res = FSR_RESOLUTION #FSR Resolution
-        self.groupwrite = dxl.groupSyncWrite(self.port, self.protocol, ADDR_GOAL_POS, LEN_GOAL_POSITION)
+        self.port_handler = port_handler
+        self.packet_handler = packet_handler
+        self.group_sync_write = GroupSyncWrite(self.port_handler, self.packet_handler, ADDR_GOAL_POS, LEN_GOAL_POSITION)
         # self.groupspeed = dxl.groupSyncWrite(self.port, self.protocol, ADDR_MOV_SPEED, LEN_MOV_SPEED)
         # self.groupread = dxl.groupSyncRead(self.port, self.protocol, ADDR_PRES_POS, LEN_PRESENT_POSITION)
-        print("Protocol 1 ready")
 
     # Check for errors in the last comm
-    def check_error(self, id):
-        error = dxl.getLastRxPacketError(self.port, self.protocol)
-        if error != 0:
-            print("[id: %d, %s]" %(id, dxl.getRxPacketError(self.protocol, error)))
+    def check_comm_error(self, id, dxl_comm_result, dxl_error):
+        if dxl_comm_result != COMM_SUCCESS:
+            print("[id: %d, %s]" %(id, self.packet_handler.getTxRxResult(dxl_comm_result)))
+            return False
+        elif dxl_error != 0:
+            print("[id: %d, %s]" %(id, self.packet_handler.getRxPacketError(dxl_error)))
             return False
         return True
 
-
-    def check_result(self, id):
-        comm_result = dxl.getLastTxRxResult(self.port, self.protocol)
-        if comm_result != COMM_SUCCESS:
-            print("[id: %d, %s]" %(id, dxl.getTxRxResult(self.protocol, comm_result)))
-            return False
-        return True
-
-
+    # Enable/Disable the torque of dyanmixel
     def set_torque_status(self, ids, value):
         for id in ids:
             dxl.write1ByteTxRx(self.port, self.protocol, id, ADDR_TORQUE_ENABLE, value)
             self.check_result(id)
             self.check_error(id)
 
+    # Convert bits to degrees
     def to_degree(self, value):
         angle = int(value*self.mx_res)
         return angle
 
+    # Convert degrees to bits
     def from_degree(self, angle):
         degree = int(float(angle)/self.mx_res)
         return degree
 
-    def set_moving_speed(self, vel_dict):
-        for id, vel in vel_dict.items():
-            dxl.write2ByteTxRx(self.port, self.protocol, id, vel,  ADDR_MOV_SPEED)
-            self.check_result(id)
-            self.check_error(id)
+    # Set the moving speed of the dynamixel motors
+    def set_moving_speed(self, speed_dict):
+        for id, speed in speed_dict.items():
+            self.packet_handler.write2ByteTxRx(self.port_handler, id, speed,  ADDR_MOV_SPEED)
+            self.check_comm_error(id)
 
-    def is_moving(self,ids):
-        mov = {}
-        for id in ids:
-            val = dxl.read1ByteRx(self.port, self.protocol, id, ADDR_MOVING)
-            mov[id] = val
-        return mov
+    # Check if dynamixel is moving
+    def is_moving(self, id_list):
+        moving = {}
+        for id in id_list:
+            value = self.packet_handler.read1ByteRx(self.port_handler, id, ADDR_MOVING)
+            moving[id] = value
+        return moving
 
-    def set_goal_position(self, write_dict):
+    # Set the goal position for the dynamixels in the chain
+    def set_goal_position(self, angle_dict):
         #self.set_torque_status(write_dict.keys(),1)
-        for id,angle in write_dict.items():
-            angle = self.from_degree(angle+180)
+        for id, angle in angle_dict.items():
+            # Get position from angle
+            goal_position = self.from_degree(angle + 180)
+            # generate list of byte values from position values
+            param_goal_psosition = [DXL_LOBYTE(DXL_LOWORD(goal_position)), DXL_HIBYTE(DXL_LOWORD(goal_position)), DXL_LOBYTE(DXL_HIWORD(goal_position)), DXL_HIBYTE(DXL_HIWORD(goal_position))]
             # Add dxl goal position value to the Syncwrite storage
-            addparam_result = ctypes.c_ubyte(dxl.groupSyncWriteAddParam(self.groupwrite, id, angle, LEN_GOAL_POSITION)).value
-            if addparam_result != 1:
+            addparam_result = self.group_sync_write.addParam(id, param_goal_position))
+            if addparam_result != True:
                 print("[ID:%03d] groupSyncWrite addparam failed" % (id))
                 quit()
-
         # Syncwrite goal position
-        dxl.groupSyncWriteTxPacket(self.groupwrite)
-        self.check_result(id)
-
+        self.group_sync_write.txPacket()
         # Clear syncwrite parameter storage
-        dxl.groupSyncWriteClearParam(self.groupwrite)
-        self.check_result(id)
-        
+        self.group_sync_write.clearParam()
+
+    # Get the present dynamixel positions
     def get_present_position(self, ids):
         positions = []
         for id in ids:
-            present_position = dxl.read2ByteTxRx(self.port, self.protocol, id, ADDR_PRES_POS)
+            present_position = self.packet_handler.read2ByteTxRx(self.port_handler, id, ADDR_PRES_POS)
             # if not self.check_result() and not self.check_error():
             present_position = self.to_degree(present_position)-180
             positions.append(present_position)
         return dict(zip(ids,positions))
-    
-    def to_newton(self,value):
-        newton = float(value)*self.fsr_res
-        return newton    
 
+    # Convert fsr readings to newtons
+    def to_newton(self, value):
+        newton = float(value)*self.fsr_res
+        return newton
+
+    # Read FSR values
     def get_fsr_readings(self, foot):
-        id_dic = {'left':111,'right':112}
-        id = id_dic[foot]
+        id_dict = {'left':111,'right':112}
+        id = id_dict[foot]
         #fsr_reading = {'1':0, '2':0, '3':0, '4':0, 'x':0, 'y':0}
         fsr_reading = {}
         fsr_reg1 = {'1':ADDR_FSR_1, '2':ADDR_FSR_2, '3':ADDR_FSR_3, '4':ADDR_FSR_4}
         fsr_reg2 = {'x':ADDR_FSR_X, 'y':ADDR_FSR_Y}
-        
-        for reg in fsr_reg1.keys():
-            fsr_reading[reg] = self.to_newton(dxl.read2ByteTxRx(self.port, self.protocol, id, fsr_reg1[reg]))
-            self.check_result(id)
-            self.check_error(id)
-        for reg in fsr_reg2.keys():
-            fsr_reading[reg] = dxl.read1ByteTxRx(self.port, self.protocol, id, fsr_reg2[reg])
-            self.check_result(id)
-            self.check_error(id)
-        
-        return fsr_reading 
-    
 
+        for reg in fsr_reg1.keys():
+            fsr_reading[reg] = self.to_newton(self.packet_handler.read2ByteTxRx(self.port_handler, id, fsr_reg1[reg]))
+            self.check_comm_error(id)
+        for reg in fsr_reg2.keys():
+            fsr_reading[reg] = self.packet_handler.read1ByteTxRx(self.port_handler, id, fsr_reg2[reg])
+            self.check_comm_error(id)
+        return fsr_reading
